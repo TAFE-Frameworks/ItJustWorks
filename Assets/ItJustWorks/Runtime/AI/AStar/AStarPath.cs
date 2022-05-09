a875bf6b17cf97a7fedf5900f13d02e298344eb3
JamesMillsTAFE <james.mills39@tafensw.edu.au>
2022-05-09 11:44:28 +1000

    AStar algorithm implemented

---
+++ Assets/ItJustWorks/Runtime/AI/AStar/AStarPath.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace ItJustWorks.AI.AStar
+{
+	public class AStarPath
+	{
+		private readonly List<Vector3> path;
+
+		private int index;
+		private float progress;
+
+		public AStarPath()
+		{
+			path = new List<Vector3>();
+			index = 0;
+			progress = 0;
+		}
+
+		public void Add(Vector3 _node) => path.Add(_node);
+
+		public void Reverse() => path.Reverse();
+
+		private void ProgressToNextNode()
+		{
+			index++;
+			progress = 0;
+		}
+
+		public void Visualise(LineRenderer _pathRenderer)
+		{
+			_pathRenderer.positionCount = path.Count;
+
+			for(int i = 0; i < path.Count; i++)
+			{
+				_pathRenderer.SetPosition(i, path[i]);
+			}
+		}
+	}
+}
---
+++ Assets/ItJustWorks/Samples/AI/Pathfinding/AStarSample.cs
@@ -0,0 +1,32 @@
+﻿using ItJustWorks.AI.AStar;
+
+using UnityEngine;
+
+namespace ItJustWorks.Samples.AI.Pathfinding
+{
+	public class AStarSample : MonoBehaviour
+	{
+		[SerializeField] private LineRenderer pathRenderer;
+
+		private Vector3 initialClick = Vector3.positiveInfinity;
+
+		private void Update()
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+				{
+					if(initialClick == Vector3.positiveInfinity)
+					{
+						initialClick = hit.point;
+					}
+					else
+					{
+						AStarManager.Instance.FindPath(initialClick, hit.point).Visualise(pathRenderer);
+						initialClick = Vector3.positiveInfinity;
+					}
+				}
+			}
+		}
+	}
+}
